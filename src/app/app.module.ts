3731accd8f089368e23a37d10019d045fee125d1
wendellmccaulay@hotmail.com <wendellmccaulay@hotmail.com>
2018-02-14 11:17:22 -0300

    Creation of the project with Materialize CSS Framework

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { MaterializeModule } from 'angular2-materialize';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    MaterializeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
