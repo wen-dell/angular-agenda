b6aadc44728be00e6f9dc83116ec4c2580510d43
wendellmccaulay@hotmail.com <wendellmccaulay@hotmail.com>
2018-02-14 12:07:32 -0300

    Main components created

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from '@angular/router';
+
+
+import { ContactListComponent } from "./contact-list/contact-list.component";
+import { ContactFormComponent } from "./contact-form/contact-form.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
+
+
+const appRoutes: Routes = [
+    { path: 'contacts', component: ContactListComponent },
+    { path: 'contact/new', component: ContactFormComponent },
+    { path: '', redirectTo: 'contacts', pathMatch: 'full' },
+    { path: '**', component: NotFoundComponent }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(appRoutes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MaterializeModule } from 'angular2-materialize';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactListComponent,
+    ContactFormComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    MaterializeModule
+    MaterializeModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
